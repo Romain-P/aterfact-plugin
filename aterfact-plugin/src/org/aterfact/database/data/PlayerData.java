c99f0ab4a5a3d8e5c6ec52b698d27e87a1e2e257
John-r <skryn01@gmail.com>
2014-04-24 12:20:58 +0200

    Refactoring player object

--- aterfact-plugin/src/org/aterfact/database/data/PlayerData.java
+++ aterfact-plugin/src/org/aterfact/database/data/PlayerData.java
@@ -2,7 +2,7 @@ package org.aterfact.database.data;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aterfact.database.AbstractDAO;
-import org.aterfact.database.objects.Player;
+import org.aterfact.objects.Player;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- aterfact-plugin/src/org/aterfact/objects/Player.java
+++ aterfact-plugin/src/org/aterfact/objects/Player.java
@@ -1,4 +1,4 @@
-package org.aterfact.database.objects;
+package org.aterfact.objects;
 
 import lombok.Getter;
 import lombok.Setter;
